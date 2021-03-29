8c51f3fc935c4dd187a7962b8558d6ad1d6a03bd
Helmut Josue Colindres Brenes <helmut-1521@hotmail.com>
2021-03-28 19:20:48 -0600

    Diseño de la interfaz

---
+++ app/src/main/java/com/helcode/ejecicios_practicos_kotlin/MainConvertidor.kt
@@ -0,0 +1,14 @@
+package com.helcode.ejecicios_practicos_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainConvertidor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_convertidor)
+    }
+
+    fun convertir(view: View) {}
+}
